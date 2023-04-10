8582765686f39c2053bc25103f270965d378baf9
TropikoW <jovannykr@hotmail.com>
2023-04-10 08:38:10 -0500

    the color black i like more

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const buttonInitiate = document.querySelector('#button-initiate');
-const buttonUs = document.querySelector('#button-us');
+const buttonAbout = document.querySelector('#button-about');
 const buttonClient = document.querySelector('#button-client');
 const buttonContact = document.querySelector('#button-contact');
 const buttonContactFirst = document.querySelector('#contact-first');
+const modal = document.querySelector('#modal');
+const buttonClose = document.querySelector('#btns__close');
+const btnsFormSend = document.querySelector('#btns-form-send');
 
-buttonClient.addEventListener('click',messageAlert);
-buttonContact.addEventListener('click',messageAlert);
-buttonContactFirst.addEventListener('click',messageAlert);
+buttonClient.addEventListener('click',showModal);
+buttonContact.addEventListener('click',showModal);
+buttonContactFirst.addEventListener('click',showModal);
+buttonClose.addEventListener('click',closeModal);
+btnsFormSend.addEventListener('click',showModal);
 
-function messageAlert() {
-    alert('Hi!, we no have for the moment site for this link, please wait');
+function showModal(e) {
+    e.preventDefault();
+    modal.classList.add('modal__show');
 };
+
+function closeModal(e) {
+    e.preventDefault();
+    modal.classList.remove('modal__show');
+}
